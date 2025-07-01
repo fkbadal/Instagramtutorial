c0ef704dd3fb32f5b7483e0a72520198c8fe5332
fkbadal <fkbadal6006@gmail.com>
2025-07-01 04:59:06 -0400

    Initial Commit

---
+++ Instagramtutorial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Instagramtutorial
+//
+//  Created by Fayzul Kobir Badal on 7/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Instagramtutorial/InstagramtutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramtutorialApp.swift
+//  Instagramtutorial
+//
+//  Created by Fayzul Kobir Badal on 7/1/25.
+//
+
+import SwiftUI
+
+@main
+struct InstagramtutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
